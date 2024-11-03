c9c2867e082e71e3c6009843c8ebf1e0db2c274f
pierre schuvey <chromatykretro@gmail.com>
2024-11-03 22:59:19 +0100

    fix

--- src/component/home.js
+++ src/component/home.js
@@ -15,15 +15,14 @@ function HomePage(props) {
                 tous les 2h en plus de ceux dispo avec les tokens ( 300 points de chaine ), rejoins le discord si tu as
                 des questions et hésite pas à claquer ton follow sur twitch !</p>
             <div className="socialContainer">
-                <p className="myNetworks">Mes reseaux</p>
                 <a className="socialLink" target='_blank' href="https://discord.gg/8V6fyQdSCG"><i
-                    class="fa-brands fa-discord"></i>Discord</a>
+                    class="fa-brands fa-discord"></i></a>
                 <a className="socialLink" target='_blank' href="https://twitch.tv/chromatyk"><i
-                    class="fa-brands fa-twitch"></i>Twitch</a>
+                    class="fa-brands fa-twitch"></i></a>
                 <a className="socialLink" target='_blank' href="https://twitter.com/Chromatyk_"><i
-                    class="fa-brands fa-twitter"></i>Twitter</a>
+                    class="fa-brands fa-twitter"></i></a>
                 <a className="socialLink" target='_blank' href="https://www.instagram.com/chromatyk_/"><i
-                    class="fa-brands fa-instagram"></i>Instagram</a>
+                    class="fa-brands fa-instagram"></i></a>
             </div>
         </div>
     </>

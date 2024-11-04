69ae21f77ecbd17e45b2453d5a28b11c5766063b
pierre schuvey <chromatykretro@gmail.com>
2024-11-04 13:04:53 +0100

    fix

--- src/component/profil.js
+++ src/component/profil.js
@@ -227,7 +227,7 @@ function Profil(props) {
                                 <circle className="progress-meter" r="48" cx="60" cy="60" fill="transparent"
                                         stroke-width="20" strokeDashoffset="0"></circle>
                                 <circle className="progress-value" r="48" cx="60" cy="60" fill="transparent"
-                                        stroke-width="20" strokeDashoffset={parseFloat(profil[0].xp / (profil[0].level * 500) * 300).toFixed(2)} stroke-dasharray="301.59"></circle>
+                                        stroke-width="20" style={{strokeDashoffset:parseFloat(profil[0].xp / (profil[0].level * 500) * 300).toFixed(2)}} stroke-dasharray="301.59"></circle>
                             </svg>
                             <span>
                                 <button onClick={handleProfileImage} style={{

267fa5188e2e3e60e7f8d1bbabe56de4dabce7a6
pierre schuvey <chromatykretro@gmail.com>
2024-11-06 11:22:04 +0100

    fix

--- src/component/profil.js
+++ src/component/profil.js
@@ -373,7 +373,7 @@ function Profil(props) {
                                 <>
                                     <div className={"profilVisualsCard"}>
                                         <Tooltip style={{zIndex: "1"}} anchorSelect=".anchorTooltip"/>
-                                        <img style={{width: "60px"}} className="anchorTooltip"
+                                        <img style={{width: "80px"}} className="anchorTooltip"
                                              data-tooltip-content={pourcentCard == 100 ? "100% du Cartodex Complété" : pourcentCard >= 80 ? "80% du Cartodex Complété" : pourcentCard >= 60 ? "60% du Cartodex Complété" : pourcentCard >= 40 ? "40% du Cartodex Complété" : pourcentCard >= 20 ? "20% du Cartodex Complété" : "Au moins 1 carte obtenue"}
                                              src={pourcentCard == 100 ? Lv6c : pourcentCard >= 80 ? Lv5c : pourcentCard >= 60 ? Lv4c : pourcentCard >= 40 ? Lv3c : pourcentCard >= 20 ? Lv2c : Lv1c}/>
                                     </div>

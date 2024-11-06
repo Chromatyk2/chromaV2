7ee764f1d71f002db9254c25f123ad91413b1227
pierre schuvey <chromatykretro@gmail.com>
2024-11-06 13:28:19 +0100

    fix

--- src/component/openingCards.js
+++ src/component/openingCards.js
@@ -377,13 +377,13 @@ function OpeningCards(props) {
                                                      style={{display: key < 9 && "none"}}
                                                      id={"cardNb" + key} keyCard={key}
                                                      cardId={val.id}
-                                                     onClick={key == 0 ? getLastCard : getCard}
+                                                     onClick={key == 0 ? getLastCard : getToken === true && key == 1 ? getNextToken : getCard}
                                                      className={isHidden === true ? "fit-picture dropCards hiddenCards" : endPull === true ? "fit-picture dropCards showCards endPull" : key == 9 ? "fit-picture dropCards showCards glowGet" : "fit-picture dropCards glowGet"}
                                                      src={"https://images.pokemontcg.io/" + val.set.id + "/" + val.number + ".png"}
                                                      onError={errorImage}
                                                      alt="Grapefruit slice atop a pile of other slices"/>
                                                 {getToken === true &&
-                                                    endPull === true &&
+                                                    isToken === true &&
                                                     <img
                                                         rarity={4}
                                                         onClick={getLastCard}

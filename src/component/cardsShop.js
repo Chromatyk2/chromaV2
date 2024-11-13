a31d4d415f0e41f41e8dcfad7067217cec594ab8
pierre schuvey <chromatykretro@gmail.com>
2024-11-13 12:42:08 +0100

    fix

--- src/component/cardsShop.js
+++ src/component/cardsShop.js
@@ -127,7 +127,6 @@ function CardsShop(props) {
                                         })
                                     })
                             })
-                    })
     }, [])
 
     function registerCards(e) {

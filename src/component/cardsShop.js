be5375233ef12571a6b8d4af904183122c8f77ec
pierre schuvey <chromatykretro@gmail.com>
2024-11-14 00:27:10 +0100

    fix

--- src/component/cardsShop.js
+++ src/component/cardsShop.js
@@ -345,7 +345,7 @@ function CardsShop(props) {
         setOnOpen(false);
     }
     function changeCarousel(e) {
-        console.log(e);
+        items[0].display = "none"
     }
     return (
         <>

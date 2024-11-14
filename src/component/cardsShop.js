307cae2a6e86ddef6c2b0cb0ee416763686a576b
pierre schuvey <chromatykretro@gmail.com>
2024-11-14 01:44:36 +0100

    fix

--- src/component/cardsShop.js
+++ src/component/cardsShop.js
@@ -147,69 +147,10 @@ function CardsShop(props) {
     function selectGen(e) {
         setArray([])
         setSelectedBoosterId(0)
-        if(e.target.value == "all"){
             Axios
                 .get("/api/getBoostersList")
                 .then(function(response){
-                    setItems(response.data);
-
-                    response.data.map((val, key) => {
-                        setArray(array => [...array,
-                            <div className="uniqueTradeContainer">
-                                <div className={"containerImgBooster"}>
-                                    <LazyLoadImage
-                                        delayTime={0}
-                                        threshold={200}
-                                        placeholderSrc={"https://images.pokemontcg.io/defaut.png"}
-                                        width={"150"}
-                                        style={{width: "220px", filter: "brightness(1) drop-shadow(2px 4px 6px black)",borderRadius: "25px"}}
-                                        effect="opacity"
-                                        delayTime={5}
-                                        wrapperProps={{
-                                            // If you need to, you can tweak the effect transition using the wrapper style.
-                                            style: {transitionDelay: "0.1s"},
-                                        }}
-                                        src={"/Boosters/" + val.name + ".png"}/>
-                                    {/*<img className="fit-picture"*/}
-                                    {/*     src={"https://images.pokemontcg.io/" + val.name + "/logo.png"}*/}
-                                    {/*     alt="Grapefruit slice atop a pile of other slices"/>*/}
-                                </div>
-                                {points > 0 ?
-                                    loading === false ?
-                                        <div>
-
-                                            <button value={val.name}
-                                                    onClick={openModal}
-                                                    className="guessTradeButton">Ouvrir
-                                            </button>
-                                        </div>
-                                        :
-                                        <div>
-                                            <button className="guessTradeButton">Chargement</button>
-                                        </div>
-                                    :
-                                    <div>
-                                        <button className="guessTradeButton">Aucun Token</button>
-                                    </div>
-                                }
-                                {canOpenLive == 1 &&
-                                    <div>
-
-                                        <button value={val.name}
-                                                onClick={freeBooster}
-                                                className="guessTradeButton">Booster Gratuit
-                                        </button>
-                                    </div>
-                                }
-                            </div>
-                        ]);
-                    })
-                })
-        }else{
-            Axios
-                .get("/api/getBoostersList")
-                .then(function(response){
-                    setItems(response.data);
+                    setItems(response.data.filter(item => item.gen == e.target.value));
                     response.data.filter(item => item.gen == e.target.value).map((val, key) => {
                         setArray(array => [...array,
                             <div className="uniqueTradeContainer">
@@ -262,7 +203,6 @@ function CardsShop(props) {
                         ]);
                     })
                 })
-        }
     }
     function openModal(e) {
         setOnOpen(true);
